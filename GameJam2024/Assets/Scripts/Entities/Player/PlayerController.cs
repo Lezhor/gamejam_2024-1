b7459e09ad225fbd548ec474a2c12091f8398fcc
Lezhor <117937256+Lezhor@users.noreply.github.com>
2024-02-14 00:08:23 +0100

    currentTile is swapped randomly

--- GameJam2024/Assets/Scripts/Entities/Player/PlayerController.cs
+++ GameJam2024/Assets/Scripts/Entities/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using GameLogic;
+using GameLogic.player;
 using GameLogic.world;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -15,6 +16,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] 
     private TileData currentTile;
 
+    public TileData CurrentTile
+    {
+        get
+        {
+            return currentTile;
+        }
+
+        set
+        {
+            currentTile = value;
+            // TODO - Call Event to update UI
+        }
+    }
+
+    private TileRandomizer _tileRandomizer;
+
     private Rigidbody2D _rigidbody;
 
     private Camera _cam;
@@ -25,6 +42,7 @@ public class PlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         _cam = GameManager.Instance.Cam;
         _world = GameManager.Instance.World;
+        _tileRandomizer = new TileRandomizer(GameManager.Instance.Tiles);
     }
 
     private void Update()
@@ -35,13 +53,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private bool PlaceActiveTileOnCursorField()
+    private void PlaceActiveTileOnCursorField()
     {
         Vector3 mousePos = Input.mousePosition;
         Vector3 mouseWorldPos = _cam.ScreenToWorldPoint(mousePos);
         Vector2Int tilePos = new Vector2Int(Mathf.FloorToInt(mouseWorldPos.x), Mathf.FloorToInt(mouseWorldPos.y));
 
-        return _world.PlaceIfPossible(tilePos.x, tilePos.y, currentTile);
+        if (_world.PlaceIfPossible(tilePos.x, tilePos.y, CurrentTile))
+        {
+            CurrentTile = _tileRandomizer.GetRandomTile();
+        }
         
     }
 
---
+++ GameJam2024/Assets/Scripts/GameLogic/player/TileRandomizer.cs
@@ -0,0 +1,40 @@
+﻿using GameLogic.world;
+using UnityEngine;
+
+namespace GameLogic.player
+{
+    public class TileRandomizer
+    {
+        private readonly TileRegistry _registry;
+        
+        public TileRandomizer(TileRegistry registry)
+        {
+            _registry = registry;
+        }
+
+        /**
+         * Random tile with equal probabilities (No Dead Ends)
+         */
+        public TileData GetRandomTile()
+        {
+            return Random.Range(0, 11) switch
+            {
+                0 => _registry.we,
+                1 => _registry.ns,
+                
+                2 => _registry.nw,
+                3 => _registry.ws,
+                4 => _registry.se,
+                5 => _registry.ne,
+                
+                6 => _registry.nws,
+                7 => _registry.wse,
+                8 => _registry.nse,
+                9 => _registry.nwe,
+                
+                10 => _registry.nwse,
+                _ => null
+            };
+        }
+    }
+}
