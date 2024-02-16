d9b272771a3c02bdebe9e729a43c4635fbd93091
Lezhor <117937256+Lezhor@users.noreply.github.com>
2024-02-16 23:48:20 +0100

    Added DebugAction and verified - Everyting works!!!

---
+++ GameJam2024/Assets/Scripts/GameLogic/world/tiles/actions/DebugTileAction.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace GameLogic.world.tiles.actions
+{
+    public class DebugTileAction : TileAction
+    {
+        private readonly bool _debugSetVisible;
+        private readonly bool _debugSetExplored;
+        private readonly bool _debugPlayerEnter;
+        private readonly bool _debugPlayerExit;
+        private readonly bool _debugPerformAction;
+
+        public string Tag => "DebugTile{ ( " + Tile.Pos.x + " | " + Tile.Pos.y + " ) }:  ";
+
+        public DebugTileAction(WorldTile tile, bool debugSetVisible, bool debugSetExplored, bool debugPlayerEnter, bool debugPlayerExit, bool debugPerformAction) : base(tile)
+        {
+            _debugSetVisible = debugSetVisible;
+            _debugSetExplored = debugSetExplored;
+            _debugPlayerEnter = debugPlayerEnter;
+            _debugPlayerExit = debugPlayerExit;
+            _debugPerformAction = debugPerformAction;
+        }
+
+        public override void OnSetVisibility(bool state)
+        {
+            if (_debugSetVisible)
+            {
+                Debug.Log(Tag + "Visibility set to " + state);
+            }
+        }
+
+        public override void OnSetExplored(bool state)
+        {
+            if (_debugSetExplored)
+            {
+                Debug.Log(Tag + "Explored set to " + state);
+            }
+        }
+
+        public override void OnPlayerEnterTile()
+        {
+            if (_debugPlayerEnter)
+            {
+                Debug.Log(Tag + "Player Entered Tile!");
+            }
+        }
+
+        public override void OnPlayerExitTile()
+        {
+            if (_debugPlayerExit)
+            {
+                Debug.Log(Tag + "Player Exited Tile!");
+            }
+        }
+
+        protected override void PerformAction()
+        {
+            if (_debugPerformAction)
+            {
+                Debug.Log(Tag + "Action performed!");
+            }
+        }
+    }
+}
---
+++ GameJam2024/Assets/Scripts/GameLogic/world/tiles/actions/DebugTileActionFactory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GameLogic.world.tiles.actions
+{
+    [CreateAssetMenu(fileName = "New Debug Tile Action", menuName = "World/Actions/Debug Tile Action")]
+    public class DebugTileActionFactory : TileActionFactory
+    {
+        [SerializeField] 
+        private bool debugSetVisible = true;
+        [SerializeField] 
+        private bool debugSetExplored = true;
+        [SerializeField] 
+        private bool debugPlayerEnter = true;
+        [SerializeField] 
+        private bool debugPlayerExit = true;
+        [SerializeField] 
+        private bool debugPerformAction = true;
+        
+        public override TileAction CreateAction(WorldTile tile)
+        {
+            return new DebugTileAction(tile, debugSetVisible, debugSetExplored,
+                debugPlayerEnter, debugPlayerExit, debugPerformAction);
+        }
+    }
+}
